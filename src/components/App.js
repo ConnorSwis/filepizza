116639e57694212df857cb2484daeb9ade62dc75
ConnorSwis <condude13@gmail.com>
2024-07-07 14:14:27 -0400

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,7 @@ import FrozenHead from "react-frozenhead";
 import React from "react";
 import SupportStore from "../stores/SupportStore";
 import { RouteHandler } from "react-router";
-import ga from "react-google-analytics";
 
-if (process.env.GA_ACCESS_TOKEN) {
-  ga("create", process.env.GA_ACCESS_TOKEN, "auto");
-  ga("send", "pageview");
-}
 
 export default class App extends React.Component {
   constructor() {
@@ -35,8 +30,6 @@ export default class App extends React.Component {
         <FrozenHead>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="monetization" content="$twitter.xrptipbot.com/kernio" />
-          <meta property="og:url" content="https://file.pizza" />
           <meta
             property="og:title"
             content="FilePizza - Your files, delivered."
@@ -47,7 +40,7 @@ export default class App extends React.Component {
           />
           <meta
             property="og:image"
-            content="https://file.pizza/images/fb.png"
+            content="/images/fb.png"
           />
           <title>FilePizza - Your files, delivered.</title>
           <link rel="stylesheet" href="/fonts/fonts.css" />
@@ -60,32 +53,7 @@ export default class App extends React.Component {
           <div className="container">
             {this.state.isSupported ? <RouteHandler /> : <ErrorPage />}
           </div>
-          <footer className="footer">
-            <p>
-              <strong>Like FilePizza?</strong> Support its development! <a href="https://commerce.coinbase.com/checkout/247b6ffe-fb4e-47a8-9a76-e6b7ef83ea22" className="donate-button">donate</a>
-            </p>
-
-            <p className="byline">
-              Cooked up by{" "}
-              <a href="http://kern.io" target="_blank">
-                Alex Kern
-              </a>{" "}
-              &amp;{" "}
-              <a href="http://neeraj.io" target="_blank">
-                Neeraj Baid
-              </a>{" "}
-              while eating <strong>Sliver</strong> @ UC Berkeley &middot;{" "}
-              <a href="https://github.com/kern/filepizza#faq" target="_blank">
-                FAQ
-              </a>{" "}
-              &middot;{" "}
-              <a href="https://github.com/kern/filepizza" target="_blank">
-                Fork us
-              </a>
-            </p>
-          </footer>
           <script>FilePizza()</script>
-          { process.env.GA_ACCESS_TOKEN ? <ga.Initializer /> : <div></div> }
         </body>
       </html>
     );
